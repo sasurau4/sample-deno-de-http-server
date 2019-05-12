d0e4a9e40325f2cf6dc85aea50ddc70a542040d9
Daiki Ihara <sasurau4@gmail.com>
2019-05-12 13:39:36 +0900

    prettiered

--- echo_client.ts
+++ echo_client.ts
@@ -1,14 +1,13 @@
 async function main() {
-  const conn = await Deno.dial("tcp", "127.0.0.1:8888")
+  const conn = await Deno.dial('tcp', '127.0.0.1:8888');
 
-  const encoder = new TextEncoder()
-  const decoder = new TextDecoder()
+  const encoder = new TextEncoder();
+  const decoder = new TextDecoder();
 
-  await conn.write(encoder.encode("hello!"))
-  const buffer = new Uint8Array(1024)
-  const {eof, nread} = await conn.read(buffer)
-  const reply = decoder.decode(buffer.slice(0,nread))
-  console.log(reply)
+  await conn.write(encoder.encode('hello!'));
+  const buffer = new Uint8Array(1024);
+  const { eof, nread } = await conn.read(buffer);
+  const reply = decoder.decode(buffer.slice(0, nread));
+  console.log(reply);
 }
-main()
-
+main();
--- echo_server.ts
+++ echo_server.ts
@@ -1,15 +1,15 @@
 async function main() {
-  const listener = Deno.listen("tcp", "127.0.0.1:8888")
+  const listener = Deno.listen('tcp', '127.0.0.1:8888');
 
   const conn = await listener.accept();
 
-  const buffer = new Uint8Array(1024)
+  const buffer = new Uint8Array(1024);
 
-  const {eof, nread} = await conn.read(buffer)
+  const { eof, nread } = await conn.read(buffer);
 
-  await conn.write(buffer.slice(0, nread))
+  await conn.write(buffer.slice(0, nread));
 
-  conn.close()
+  conn.close();
 }
 
-main()
+main();
